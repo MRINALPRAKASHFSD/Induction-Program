121f519b3349e2038d823a2b9e31ff786b7ffdeb
MRINALPRAKASHFSD <212330473+MRINALPRAKASHFSD@users.noreply.github.com>
2026-05-20 16:35:51 +0000

    Implement form validation with react-hook-form

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -103,10 +103,10 @@ function RootShell({ children }: { children: React.ReactNode }) {
 
 function RootComponent() {
   const { queryClient } = Route.useRouteContext();
-
   return (
     <QueryClientProvider client={queryClient}>
       <Outlet />
+      <Toaster richColors position="top-center" />
     </QueryClientProvider>
   );
 }
